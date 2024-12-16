98d8816bc2b08330133b854c67f7ae5ea9cdf42c
hericlibong <hericlibong@gmail.com>
2024-12-16 07:26:48 +0100

    added unitests for letting, address & profil

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+class ProfileTestCase(TestCase):
+    def test_profile_str(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+        profile = Profile.objects.create(user=user, favorite_city='Paris')
+        self.assertEqual(str(profile), 'testuser')
