9122ecf0e28b4da10094277d2289a25b8d9df311
hericlibong <hericlibong@gmail.com>
2024-12-16 20:38:36 +0100

    added namespace and change name for lettings

--- config/urls.py
+++ config/urls.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 # from profiles.views import profiles_index, profile
-from lettings.views import lettings_index, letting
+# from lettings.views import lettings_index, letting
 from oc_lettings_site.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('lettings/', lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', letting, name='letting'),
+    path('lettings/', include('lettings.urls', namespace='lettings')),
+    # path('lettings/<int:letting_id>/', letting, name='letting'),
+    # path('lettings/', lettings_index, name='lettings_index'),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     # path('profiles/', profiles_index, name='profiles_index'),
     # path('profiles/<str:username>/', profile, name='profile'),
