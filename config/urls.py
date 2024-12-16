b561320c6215d5fc4fc3e63074279b3f82f849aa
hericlibong <hericlibong@gmail.com>
2024-12-16 21:04:17 +0100

    added namespace for oc_lettings_site

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# from profiles.views import profiles_index, profile
-# from lettings.views import lettings_index, letting
-from oc_lettings_site.views import index
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', include('oc_lettings_site.urls', namespace='oc_lettings_site')),
     path('lettings/', include('lettings.urls', namespace='lettings')),
-    # path('lettings/<int:letting_id>/', letting, name='letting'),
-    # path('lettings/', lettings_index, name='lettings_index'),
     path('profiles/', include('profiles.urls', namespace='profiles')),
-    # path('profiles/', profiles_index, name='profiles_index'),
-    # path('profiles/<str:username>/', profile, name='profile'),
+    
 ]
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,8 +1,8 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# app_name = 'oc_lettings_site'
+app_name = 'oc_lettings_site'
 
-# urlpatterns = [
-#     path('', views.index, name='index'),
-# ]
+urlpatterns = [
+    path('', views.index, name='index'),
+]
