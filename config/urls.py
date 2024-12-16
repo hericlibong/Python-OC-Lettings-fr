59f32ea4d317d222f352144a3f232a6ed73f372e
hericlibong <hericlibong@gmail.com>
2024-12-16 19:57:23 +0100

    change views, add namespace, change template for profiles

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from profiles.views import profiles_index, profile
+# from profiles.views import profiles_index, profile
 from lettings.views import lettings_index, letting
 from oc_lettings_site.views import index
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('lettings/', lettings_index, name='lettings_index'),
     path('lettings/<int:letting_id>/', letting, name='letting'),
-    path('profiles/', profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', profile, name='profile'),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
+    # path('profiles/', profiles_index, name='profiles_index'),
+    # path('profiles/<str:username>/', profile, name='profile'),
 ]
