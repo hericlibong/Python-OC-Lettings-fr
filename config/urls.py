2919e6d7b7c08091a4bb6f3dfbf9d1e391ce9802
hericlibong <hericlibong@gmail.com>
2024-12-17 06:56:17 +0100

    add 404 and 500 error page

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
+# from django.conf.urls import handler404, handler500
+from django.views.defaults import page_not_found, server_error
+# from oc_lettings_site.views import cause_error
 
 
 urlpatterns = [
@@ -7,5 +11,17 @@ urlpatterns = [
     path('', include('oc_lettings_site.urls', namespace='oc_lettings_site')),
     path('lettings/', include('lettings.urls', namespace='lettings')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
-
+    path('404/', lambda request: page_not_found(request, exception=None)), # Test 404 page
+    path('500/', lambda request: server_error(request)), # Test 500 page
+    # path('error/', cause_error), Test error page
 ]
+
+# # Test error page
+# def custom_handler404(request, exception):
+#     return render(request, '404.html', status=404)
+
+# def custom_handler500(request):
+#     return render(request, '500.html', status=500)
+
+# handler404 = custom_handler404
+# handler500 = custom_handler500
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'oc_lettings_site/index.html')
+
+# def cause_error(request):
+#     """ This view raises an exception to test the error page. """
+#     raise Exception('This is a test error')
