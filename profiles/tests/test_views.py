f8379eae8d52e03d7dae40f4d03b307ef51442da
hericlibong <hericlibong@gmail.com>
2024-12-20 14:19:42 +0100

    advanced test_views.py

--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -2,6 +2,11 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from profiles.models import Profile
+from unittest.mock import patch
+import logging
+
+
+logger = logging.getLogger('profiles.views')
 
 
 class ProfilesViewsTest(TestCase):
@@ -29,3 +34,18 @@ class ProfilesViewsTest(TestCase):
         self.assertTemplateUsed(response, 'profiles/profile.html')
         self.assertContains(response, 'testuser')   # Vérifie que le nom utilisateur est affiché
         self.assertContains(response, 'Alençon')    # Vérifie que la ville favorite est affichée
+
+    
+    def test_index_view_logs_error(self):
+        """
+        Test that the index view logs an error and raises an exception when an error occurs.
+        """
+        with patch('profiles.models.Profile.objects.all', side_effect=Exception("Test exception")):
+            with self.assertLogs(logger, level='ERROR') as log:
+                with self.assertRaises(Exception):  # Vérifie que l'exception est levée
+                    self.client.get(reverse('profiles:index'))
+                # Vérifie que le log contient le message attendu
+                self.assertTrue(
+                    any("Error in profiles index view: Test exception" in message for message in log.output),
+                    f"Actual logs: {log.output}"
+                )
